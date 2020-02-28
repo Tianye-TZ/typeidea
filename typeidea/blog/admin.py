b5d7bd0c45746a99c3f11bc24ff289f9fe328506
TianzeZhang <tzzhang@aibee.com>
2020-02-28 19:30:22 +0800

    update func & add custom admin

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,3 +1,108 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
+from typeidea.custom_site import custom_site
+
+
+class PostInline(admin.TabularInline):
+    fields = ('title', 'desc')
+    extra = 1
+    model = Post
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    inlines = [PostInline, ]
+    list_display = ('name', 'status', 'is_nav', 'owner', 'created_time', 'post_count')    # 显示列表字段
+    fields = ('name', 'status', 'is_nav')    # 创建表单字段
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(CategoryAdmin, self).save_model(request, obj, form, change)
+    
+    def post_count(self, obj):
+        return obj.post_set.count()
+    post_count.short_description = '文章数量'
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'status', 'created_time')
+    fields = ('name', 'status')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+class CategoryOwnerFilter(admin.SimpleListFilter):
+    """ 自定义过滤器 只显示当前用户分类"""
+
+    title = '分类过滤器'
+    parameter_name = 'owner_category'
+
+    def lookups(self, request, model_admin):
+        return Category.objects.filter(owner=request.user).values_list('id', 'name')
+
+    def queryset(self, request, queryset):
+        category_id = self.value()
+        if category_id:
+            return queryset.filter(category_id=category_id)
+        return queryset
+
+@admin.register(Post, site=custom_site)
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+    list_display = [
+        'title', 'category', 'status',
+        'owner', 'created_time', 'operator'
+    ]
+    list_display_links = []
+
+    list_filter = [CategoryOwnerFilter]
+    search_fields = ['title', 'category__name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    #编辑页面
+    save_on_top = True
+
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('collapse',),
+            'fields': ('tag',),
+        }),
+    )
+    filter_horizontal = ('tag',)
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">编辑</a>',
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
+        )
+    operator.short_description = '操作'
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
+
+    def get_queryset(self, request):
+        qs = super(PostAdmin, self).get_queryset(request)
+        return qs.filter(owner=request.user)
