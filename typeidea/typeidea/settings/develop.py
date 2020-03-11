396d38d13c7ff0b70ace22ff06a42f590c285a70
TianzeZhang <tzzhang@aibee.com>
2020-03-11 22:51:30 +0800

    add redis cache

--- typeidea/typeidea/settings/develop.py
+++ typeidea/typeidea/settings/develop.py
@@ -12,8 +12,12 @@ MIDDLEWARE += [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dreamlab_db',
+        'USER': 'root',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': 3306,
     }
 }
 
